de5af88fdae971edef785890b2dcb7a0c87cdabe
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 09:01:57 +0200

    Update Motorrad.java

--- src/Motorrad.java
+++ src/Motorrad.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Motorrad {
+public class Motorrad extends Fahrzeug {
+    private int hubraum;
+
+    public Motorrad(String marke, String modell, int baujahr, int kilometerstand, int hubraum) {
+        super(marke, modell, baujahr, kilometerstand);
+        this.hubraum = hubraum;
+    }
+    public void tanken(int liter) {
+        while (true) {
+            if(liter < 0) {
+                System.out.println("Bitte geben Sie eine positive Zahl zum tanken ein.");
+            }
+            else {
+                break;
+
+            }
+        }
+    System.out.println(liter + " Liter in " + getMarke() + " " + getModell() + " getankt.");
+    }
+    public void starten() {
+        System.out.println(getMarke() + " " + getModell() + " startet.");
+    }
 }
