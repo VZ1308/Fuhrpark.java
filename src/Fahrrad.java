b75784a500dd7131c946a4bd1324b35295a9d908
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 08:59:25 +0200

    Update Fahrrad.java

--- src/Fahrrad.java
+++ src/Fahrrad.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Fahrrad {
+public class Fahrrad extends Fahrzeug {
+    private int anzahlGaenge;
+
+    public Fahrrad(String marke, String modell, int baujahr, int kilometerstand, int anzahlGaenge) {
+        super(marke, modell, baujahr, kilometerstand);
+        this.anzahlGaenge = anzahlGaenge;
+    }
+    public void schalten(int gang) {
+        while (true) {
+            if(gang <= 0 || gang >= anzahlGaenge){
+                System.out.println("Bitte wählen Sie einen Gang zwischen 1 und " + anzahlGaenge + ".");
+            }
+            else {
+                break;
+
+
+            }
+        }
+        System.out.println("Schaltet auf Gang " + gang + ".");
+    }
 }
