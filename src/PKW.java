de7893611cbbd9613691c7be7b3e8e415b2c44e0
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 09:02:22 +0200

    Update PKW.java

--- src/PKW.java
+++ src/PKW.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class PKW {
+public class PKW extends Fahrzeug{
+    private int anzahlTueren;
+    private String kraftstoffart;
+
+    public PKW(String marke, String modell, int baujahr, int kilometerstand, int anzahlTueren, String kraftstoffart) {
+        super(marke, modell, baujahr, kilometerstand);
+        this.anzahlTueren = anzahlTueren;
+        this.kraftstoffart = kraftstoffart;
+    }
+    public void tanken(int liter) {
+        while (true) {
+            if (liter < 0) {
+                System.out.println("Bitte geben Sie eine positive Zahl zum tanken ein.");
+            } else {
+                break;
+
+            }
+
+        }System.out.println(liter + " Liter " + kraftstoffart + " in " + getMarke() + " " + getModell() + " getankt");
+    }
+    public void einparken() {
+        System.out.println(getMarke() + " " + getModell() + " parkt ein.");
+    }
+
 }
