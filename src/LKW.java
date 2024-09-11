229243d3710fe8fc3fb77cd99507836170d5c2f2
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 08:59:46 +0200

    Update LKW.java

--- src/LKW.java
+++ src/LKW.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LKW {
+public class LKW extends Fahrzeug {
+    private int ladekapazitaet;
+
+    public LKW(String marke, String modell, int baujahr, int kilometerstand, int ladekapazitaet) {
+        super(marke, modell, baujahr, kilometerstand);
+        this.ladekapazitaet = ladekapazitaet;
+    }
+    public void tanken(int liter) {
+        while (true) {
+            if(liter < 0) {
+                System.out.println("Bitte geben Sie eine positive Zahl zum tanken ein.");
+            }
+            else {
+                break;
+
+
+            }
+        }
+        System.out.println(liter + "Liter in " + getMarke() + " " + getModell() + "getankt.");
+    }
+
 }
