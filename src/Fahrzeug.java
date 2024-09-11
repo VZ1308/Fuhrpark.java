15440fb6e394c186f180b13e3f3e472cd26914d8
Viktoria Zoranovic <viktoria1313mx.at>
2024-09-11 08:58:36 +0200

    Initial commit

---
+++ src/Fahrzeug.java
@@ -0,0 +1,33 @@
+public abstract class Fahrzeug {
+    private String marke;
+    private String modell;
+    private int baujahr;
+    private int kilometerstand;
+    private static int anzahlFahrzeuge =  0;
+
+    public Fahrzeug(String marke, String modell, int baujahr, int kilometerstand) {
+        this.marke = marke;
+        this.modell = modell;
+        this.baujahr = baujahr;
+        this.kilometerstand = kilometerstand;
+        anzahlFahrzeuge++;
+    }
+    public void fahren(int km) {
+        this.kilometerstand+= km;
+    }
+    public int getKilometerstand() {
+        return kilometerstand;
+    }
+    public String getMarke(){
+        return marke;
+    }
+    public String getModell() {
+        return modell;
+    }
+    public int getBaujahr() {
+        return baujahr;
+    }
+    public static int getAnzahlFahrzeuge() {
+        return anzahlFahrzeuge;
+    }
+}
